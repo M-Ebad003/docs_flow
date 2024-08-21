31f6969e184381638cbd39ca6938b5eb4682a826
ebad <ebad003@gmail.com>
2024-08-22 00:05:10 +0500

    commit new changes

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,13 @@
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Document = () => {
   return (
-    <div>Document</div>
+    <div>
+    </div>
+
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,29 +4,41 @@ import { Metadata } from "next"
 
 import { cn } from "@/lib/utils"
 import { ReactNode } from "react"
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
+import Provider from "./Provider"
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 
-export const metadata : Metadata={
+export const metadata: Metadata = {
   title: 'Live Docs',
   description: 'Your go-to collaborative editor',
 }
 
-export default function RootLayout({ children }: {children: ReactNode}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          "min-h-screen font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+    appearance={{
+      baseTheme: dark,
+      variables:{colorPrimary: '#3371FF', fontSize: '16px'},
+    }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <Provider>
+          {children}
+          </Provider>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
