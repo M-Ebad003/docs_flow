3945bc3465aa461103f168a60e175fc5c4421d7d
ebad <ebad003@gmail.com>
2024-08-25 20:10:12 +0500

    commit new changes

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,16 +1,26 @@
 "use client";
 import Loader from '@/components/Loader';
-import { getClerkUsers } from '@/lib/actions/user.action';
+import { getClerkUsers, getDocumentUsers } from '@/lib/actions/user.action';
+import { useUser } from '@clerk/nextjs';
 import { ClientSideSuspense, LiveblocksProvider } from '@liveblocks/react/suspense';
 import React, { ReactNode } from 'react'
 
 const Provider = ({ children }: { children: ReactNode }) => {
+    const {user: clerkUser}=useUser();
     return (
         <LiveblocksProvider
             authEndpoint='/api/liveblocks-auth'
             resolveUsers={async ({ userIds }) => {
                 const users = await getClerkUsers({ userIds });
                 return users;
+            }}
+            resolveMentionSuggestions={async({text,roomId})=>{
+                const roomUsers=await getDocumentUsers({
+                    roomId,
+                    currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+                    text,
+                })
+                return roomUsers;
             }}>
             <ClientSideSuspense fallback={<Loader />}>
                 {children}
